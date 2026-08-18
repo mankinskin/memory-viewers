80e6de4da187f1f3bc3b5020b0e6a9ec98fe37ee
Linus Behrbohm <linusbehrbohm@web.de>
2026-08-18 15:43:02 +0200

    remove spec-viewer

--- spec-viewer/frontend/dioxus/e2e-release/shared/test-apis.ts
+++
@@ -1,4 +0,0 @@
-export {
-  getHashParam,
-  getSelectedTreeLabels,
-} from '../../../../../viewer-api/viewer-api/frontend/dioxus/e2e/test_apis';
--- spec-viewer/frontend/dioxus/e2e-release/tracing.release.spec.ts
+++
@@ -1,5 +0,0 @@
-import { registerClientLogApiSuite, registerTracingConsoleSuite } from './shared/suites';
-import { SPEC_VIEWER } from './shared/viewers';
-
-registerTracingConsoleSuite(SPEC_VIEWER);
-registerClientLogApiSuite(SPEC_VIEWER);
